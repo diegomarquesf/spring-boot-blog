6062fd6e0e3c0e872cd2d940c121b2eab74e0fc0
Diego <fariia_di@hotmail.com>
2022-05-29 12:49:40 -0300

    Insercao do Post

--- src/main/java/br/com/diego/springbootblog/resource/PostResource.java
+++ src/main/java/br/com/diego/springbootblog/resource/PostResource.java
@@ -1,13 +1,18 @@
 package br.com.diego.springbootblog.resource;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.diego.springbootblog.entities.Post;
 import br.com.diego.springbootblog.services.PostService;
@@ -35,6 +40,22 @@ public class PostResource {
 		
 		mv.addObject("post", post);
 		return mv;
+	}
+	
+	@RequestMapping(value = "/newPosts" , method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/savePost" , method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			attributes.addFlashAttribute("messagem", "Verifique se os campos obrigatórios foram preenchidos");
+			return "redirect:/blog/newPosts";
+		}
+		post.setDatePost(LocalDate.now());
+		 postService.save(post);
+		 return "redirect:/blog/posts";
 	}
 
 }
