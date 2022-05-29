2fec19b9b263049e1c07be4d84449cd6f956e0d0
Diego <fariia_di@hotmail.com>
2022-05-28 21:59:13 -0300

    Criacao Controller(Resource) - Listagem & pagina post.html

---
+++ src/main/java/br/com/diego/springbootblog/resource/PostResource.java
@@ -0,0 +1,33 @@
+package br.com.diego.springbootblog.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.diego.springbootblog.entities.Post;
+import br.com.diego.springbootblog.services.PostService;
+
+@Controller
+@RequestMapping(value = "/blog")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+	
+	@RequestMapping(value = "/posts" , method = RequestMethod.GET)
+	public ModelAndView getPosts() {
+		ModelAndView mv = new ModelAndView("posts");
+		List<Post> posts = postService.findAll();
+		
+		mv.addObject("posts", posts);
+		return mv;
+	}
+	
+
+
+}
--- src/main/java/br/com/diego/springbootblog/utils/PostsData.java
+++ src/main/java/br/com/diego/springbootblog/utils/PostsData.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,13 +26,17 @@ public class PostsData {
 		post1.setAuthor("Diego");
 		post1.setDatePost(LocalDate.now());
 		post1.setTitle("Diário");
-		post1.setText(" A vida em meio a pandemia com escasses de dinheiro" );
+		post1.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam massa, pretium eget sollicitudin et, commodo sit amet tortor. Maecenas vel varius metus. Nunc bibendum tortor purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas elementum erat at quam efficitur ultrices. Nunc fermentum feugiat accumsan."
+				+ "Fusce efficitur, libero eget accumsan dapibus, lacus nibh volutpat arcu, sed ultrices erat ex ac mi. Proin non vulputate est. Nullam mattis, ipsum euismod gravida feugiat, ex mauris elementum augue, eget auctor velit dolor quis augue. Donec et tempor mi. Donec elementum, nulla sed placerat maximus, libero nisl rhoncus ante, quis rutrum elit nulla non nulla. Nunc metus tortor, tristique ac mi malesuada, malesuada egestas risus. Duis risus diam, ullamcorper sit amet aliquet id, porttitor eu nisi. Maecenas porta accumsan scelerisque."
+				+ "Nullam libero nibh, ornare nec dictum id, tristique at ante. Maecenas sagittis fringilla mattis. Donec consequat ut mi eget consequat. Duis dapibus porta ornare. Nulla eleifend sagittis feugiat. Quisque tincidunt urna eget velit porttitor fringilla. Phasellus sit amet semper nibh. Maecenas facilisis justo lorem. Donec rhoncus lorem dolor, dictum auctor erat condimentum ut. Curabitur nec tempor elit, at vestibulum ipsum. Nam non gravida arcu. Nulla tincidunt sapien non ex venenatis, non vulputate lectus bibendum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante magna, efficitur non sapien non, accumsan tempus dolor. Suspendisse ac nunc efficitur, ullamcorper sapien quis, consectetur felis." );
 		
 		Post post2 = new Post();
 		post2.setAuthor("Daniel");
 		post2.setDatePost(LocalDate.now());
 		post2.setTitle("Sneackers");
-		post2.setText(" Hoje foi o dia de realizar a compra do meu segundo Jordan" );
+		post2.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quam massa, pretium eget sollicitudin et, commodo sit amet tortor. Maecenas vel varius metus. Nunc bibendum tortor purus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas elementum erat at quam efficitur ultrices. Nunc fermentum feugiat accumsan."
+				+ "Fusce efficitur, libero eget accumsan dapibus, lacus nibh volutpat arcu, sed ultrices erat ex ac mi. Proin non vulputate est. Nullam mattis, ipsum euismod gravida feugiat, ex mauris elementum augue, eget auctor velit dolor quis augue. Donec et tempor mi. Donec elementum, nulla sed placerat maximus, libero nisl rhoncus ante, quis rutrum elit nulla non nulla. Nunc metus tortor, tristique ac mi malesuada, malesuada egestas risus. Duis risus diam, ullamcorper sit amet aliquet id, porttitor eu nisi. Maecenas porta accumsan scelerisque."
+				+ "Nullam libero nibh, ornare nec dictum id, tristique at ante. Maecenas sagittis fringilla mattis. Donec consequat ut mi eget consequat. Duis dapibus porta ornare. Nulla eleifend sagittis feugiat. Quisque tincidunt urna eget velit porttitor fringilla. Phasellus sit amet semper nibh. Maecenas facilisis justo lorem. Donec rhoncus lorem dolor, dictum auctor erat condimentum ut. Curabitur nec tempor elit, at vestibulum ipsum. Nam non gravida arcu. Nulla tincidunt sapien non ex venenatis, non vulputate lectus bibendum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ante magna, efficitur non sapien non, accumsan tempus dolor. Suspendisse ac nunc efficitur, ullamcorper sapien quis, consectetur felis." );
 		
 		postList.add(post1);
 		postList.add(post2);
